7daf27ae93e53d402e28c5b3cd6744e2c33706c6
danielvendzur-code <daniel.vendzur@gmail.com>
2026-08-28 20:40:30 +0200

    Include all required viewports in QA board

--- scripts/montage.py
+++ scripts/montage.py
@@ -3,9 +3,10 @@ from PIL import Image, ImageDraw
 
 source = Path('artifacts/screenshots')
 states = ['owner', 'chat', 'advisor', 'result']
+viewports = ['desktop', 'mobile', 'mobile-small']
 for brand in ['mylo','ponio','two','bellcoria','biofy','anemone']:
     tiles = []
-    for viewport in ['desktop','mobile']:
+    for viewport in viewports:
         for state in states:
             image = Image.open(source / f'{brand}-{viewport}-{state}.png').convert('RGB')
             image.thumbnail((470, 330))
@@ -13,7 +14,7 @@ for brand in ['mylo','ponio','two','bellcoria','biofy','anemone']:
             canvas.paste(image, ((490-image.width)//2, 28))
             ImageDraw.Draw(canvas).text((12,8), f'{viewport} / {state}', fill='#222')
             tiles.append(canvas)
-    board = Image.new('RGB', (490*4, 370*2), '#ececec')
+    board = Image.new('RGB', (490*4, 370*len(viewports)), '#ececec')
     for index, tile in enumerate(tiles):
         board.paste(tile, ((index%4)*490, (index//4)*370))
     board.save(source / f'{brand}-qa-board.jpg', quality=91)
